ac94423c3c691f7ca9c867f7b4eb848ac1f4945b
RamonLopesQ <lopes_ramon@hotmail.com>
2024-05-27 14:43:06 -0300

    Começo do estudo de java script (variaveis, operadores, datatypes, condicionais)

---
+++ javascript/data-types-conditionals/data-types-conditionals.js
@@ -0,0 +1,44 @@
+// Data Types:
+// Strings:
+console.log("Hello world 10+5")
+console.log('Hello world 10+5')
+console.log(`Hello world ${10+5}`)
+
+// Numbers: 
+console.log(10+11)
+console.log(10.5)
+
+// Big Int:
+const bigNum = (2 ** 55) -1
+console.log(bigNum)
+
+// Booleans:
+console.log(true)
+console.log(false)
+
+// Null:
+console.log(null)
+
+// Undefined:
+console.log(undefined)
+
+// Symbol:
+let sym1 = Symbol("foo")
+console.log(sym1)
+
+// Object:
+const objCd = {faixa1:"A", faixa2:"B", faixa3:"C"}
+console.log(objCd)
+
+// ** Array:
+const arrCd = [objCd.faixa1, objCd.faixa2, objCd.faixa3]
+console.log(arrCd)
+
+// Conditionals:
+// if-else
+
+// switch-case
+
+// while-do
+
+
---
+++ javascript/js-variables-operators/variables-operators.js
@@ -0,0 +1,43 @@
+// Variables:
+let nameVari = "Hello"
+var nameVari2 = "World"
+console.log(nameVari + " " +  nameVari2)
+const nonChange = 2341313
+console.log(nonChange)
+
+// Concatenação:
+console.log("Hello" + "World")
+console.log("Hello" + "2")
+console.log("2" + 1)
+console.log(1 + "2")
+console.log(1 + "2")
+console.log(1 + 1 + "2")
+console.log("2" + 1 + 1)
+console.log(1 + "2" + 1)
+console.log(2 * "2")
+console.log(4 / "2")
+console.log("4" - 2)
+console.log("2" - "2")
+
+// Operators:
+// Basics:
+console.log(23 + 97)
+console.log(23 - 97)
+console.log(23 * 97)
+console.log(23 / 97)
+console.log(10 % 3)
+console.log(2 ** 3)
+
+// Increment & Decrement:
+let count = 0
+console.log(count++)
+console.log(count--)
+let count2 = 1
+console.log(++count2)
+console.log(--count2)
+
+// Lógicos:
+console.log("&&")
+console.log("||")
+console.log("!")
+console.log("??")
